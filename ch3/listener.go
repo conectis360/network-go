3db66527d135b9e82aaf449ceb2f084b751c0ff6
James Garcia <james.ramos@gentecontrole.com.br>
2024-10-13 23:17:19 -0300

    feat: code explanations and go routine to close the connection

--- ch3/listener.go
+++ ch3/listener.go
@@ -13,10 +13,29 @@ func TestListener(t *testing.T) {
 	defer func() { _ = listener.Close() }()
 
 	t.Logf("bound to %q", listener.Addr())
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go func(c net.Conn) {
+			defer c.Close()
+		}(conn)
+	}
 }
 
-// the net.Listen function accepts a network type, a ip address and a port separed by a collon.
+/*
+the net.Listen function accepts a network type, a ip address and a port separed by a collon.
 
-// it returns a net.Listener interface and a error interface, if is successfully the listener is bound to the ip.
+it returns a net.Listener interface and a error interface, if is successfully the listener is bound to the ip.
 
-// Binding means that the operating system has exclusively assigned the port on the given IP address to the listener.
+Binding means that the operating system has exclusively assigned the port on the given IP address to the listener.
+
+The operating system allows no other processes to listen for incoming traffic on bound ports.
+
+You should always be diligent about closing your listener gracefully by calling its Close method.
+
+
+Failure to close the listener may lead to memory leaks or deadlocks in your code, because calls to the listener’s Accept method may block
+indefinitely. Closing the listener immediately unblocks calls to the Accept method.
+*/
